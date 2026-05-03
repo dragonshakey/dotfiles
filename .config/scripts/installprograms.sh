94f42314e44eeb6e3789d912f3d7437aa55dbe0a
dragonshakey <dragonshakey@gmail.com>
2026-05-03 22:24:58 +0300

    some nice changes

--- .config/scripts/installprograms.sh
+++ .config/scripts/installprograms.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+#install pacman packages
+sudo pacman -S `cat $HOME/packages.txt`
+
 #copy etc configs to /etc
 sudo rm /etc/locale.conf
 sudo cp $ETCCONFS/locale.conf /etc
@@ -6,13 +11,19 @@ sudo cp $ETCCONFS/locale.conf /etc
 mkdir GitRepos
 mkdir Programs
 
+#Install oh my zsh
 cd $HOME/Programs
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#install yay and other aur packages
 cd $HOME/GitRepos
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
-sudo makepkg -si
+makepkg -si
 
 cd
+yay -S librewolf-bin
+
+#clean up home
+rm .bashrc .bash_logout .bash_profile .bash_history
